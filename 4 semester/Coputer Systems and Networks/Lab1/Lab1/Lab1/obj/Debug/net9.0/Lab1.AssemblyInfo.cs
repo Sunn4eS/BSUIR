66da1dcac2fee794b2cb6a31482d539b488bfbac
Sunn4eS <sashabrazhalovich2005@gmail.com>
2025-02-20 02:10:28 +0300

    Lab 1

--- 4 semester/Coputer Systems and Networks/Lab1/Lab1/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ 4 semester/Coputer Systems and Networks/Lab1/Lab1/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5a783a07e76f806591d641e4a43086e11b0655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e029c9f09422fec96740444393d60ae89412d649")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
