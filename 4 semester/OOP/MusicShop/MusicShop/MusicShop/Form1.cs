5fd4dde539bd3ab05e9308a2609b214878bf735e
Sunn4eS <sashabrazhalovich2005@gmail.com>
2025-02-20 13:46:12 +0300

    Start

--- 4 semester/OOP/MusicShop/MusicShop/MusicShop/Form1.cs
+++ 4 semester/OOP/MusicShop/MusicShop/MusicShop/Form1.cs
@@ -16,5 +16,10 @@ namespace MusicShop
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
