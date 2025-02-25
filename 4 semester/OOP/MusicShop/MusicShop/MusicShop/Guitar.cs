dce9dcc4c90e3a9eaa8c1501023e5278be4cf5e9
Sunn4eS <sashabrazhalovich2005@gmail.com>
2025-02-26 01:58:45 +0300

    OOP Utility Class and Interface

--- 4 semester/OOP/MusicShop/MusicShop/MusicShop/Guitar.cs
+++ 4 semester/OOP/MusicShop/MusicShop/MusicShop/Guitar.cs
@@ -31,9 +31,6 @@ namespace MusicShop
             StringsModel = stringsModel;
             
         }
-        
-        
-
         private void UpdatePrice(int newPrice)
         {
             Price = newPrice;
@@ -44,41 +41,5 @@ namespace MusicShop
             UpdatePrice(Price - StringsModel.Price + newStingsModel.Price);
             StringsModel = newStingsModel;
         }
-
-        // public virtual Panel Print(string imagePath, int x, int y)
-        // {
-        //     var image = new PictureBox();
-        //     image.Location = new Point(x, y);
-        //     image.Size = new Size(150, 150);
-        //     image.SizeMode = PictureBoxSizeMode.StretchImage;
-        //     image.BorderStyle = BorderStyle.Fixed3D;
-        //     if (System.IO.File.Exists(imagePath))
-        //     {
-        //         using (var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
-        //         {
-        //             image.Image = Image.FromStream(fileStream);
-        //         }
-        //     }
-        //     else
-        //     {
-        //         MessageBox.Show($"Файл не найден: {imagePath}");
-        //     }
-        //
-        //     var info = new Label();
-        //     info.Location = new Point(0, image.Bottom + 10);
-        //     info.AutoSize = true;
-        //     info.Font = new Font("Segoe UI", 12.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
-        //     info.Text = $"{Brand} {Model}\n{Price} BYN";
-        //
-        //
-        //     var panel = new Panel();
-        //     panel.Location = new Point(0, 0);
-        //     panel.AutoScroll = true;
-        //     panel.Size = new Size(image.Width, image.Height + info.Height * 3 + 10);
-        //     panel.Controls.Add(image);
-        //     panel.Controls.Add(info);
-        //
-        //     return panel;
-        // }
     }
 }
--- 4 semester/OOP/MusicShop/MusicShop/MusicShop/Strings.cs
+++ 4 semester/OOP/MusicShop/MusicShop/MusicShop/Strings.cs
@@ -24,40 +24,5 @@ namespace MusicShop
             Size[1] = size_max;
             Material = material;
         }
-        
-        // public Panel Print(string imagePath, int x, int y)
-        // {
-        //     var image = new PictureBox();
-        //     image.Location = new Point(0, 0);
-        //     image.Size = new Size(150, 150);
-        //     image.SizeMode = PictureBoxSizeMode.StretchImage;
-        //     image.BorderStyle = BorderStyle.Fixed3D;
-        //     if (System.IO.File.Exists(imagePath))
-        //     {
-        //         using (var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
-        //         {
-        //             image.Image = Image.FromStream(fileStream);
-        //         }
-        //     }
-        //     else {
-        //         MessageBox.Show($"Файл не найден: {imagePath}");
-        //     }
-        //     var info = new Label();
-        //     info.Location = new Point(0, image.Bottom + 10);
-        //     info.AutoSize = true;
-        //     info.Font = new Font("Segoe UI", 12.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
-        //     info.Text = $"{Brand} {Model}\n{Price} BYN";
-        //     
-        //     
-        //     var panel = new Panel();
-        //     panel.Location = new Point(x, y);
-        //     panel.AutoScroll = true;    
-        //     panel.Size = new Size(image.Width, image.Height + info.Height * 3 + 10);
-        //     panel.Controls.Add(image);
-        //     panel.Controls.Add(info);
-        //     
-        //     return panel;
-        // } 
-        
     }
 }
