dae4ddfef09d8d0b2dc0a3e3d2b16adbe8799e74
Sunn4eS <sashabrazhalovich2005@gmail.com>
2025-04-09 17:24:35 +0300

    WT SmartDate

--- 4 semester/Web Technologies/Lab4/ProjectLab4/task_04.php
+++ 4 semester/Web Technologies/Lab4/ProjectLab4/task_04.php
@@ -2,10 +2,10 @@
 class SmartDate {
     private $date;
     public function __construct($dateString) {
-        $this->date = $dateString;
+        $this->date = new DateTime($dateString);
     }
     public function isWeekend(): bool {
-        $dayOfWeek = date('N', strtotime($this->date));
+        $dayOfWeek = (int)$this->date->format('N');
         return $dayOfWeek >= 6;
     }
 
@@ -17,11 +17,23 @@ class SmartDate {
                 return $interval->days;
             case 'months':
                 return $interval->y * 12 + $interval->m;
-                case 'years':
-                    return $interval->y;
-
+            case 'years':
+                return $interval->y;
+            default:
+                throw new InvalidArgumentException("Неподдерживаемая единица измерения: $unit");
         }
+    }
+    public function isLeapYear(): bool {
+        $year = (int)$this->date->format('Y');
+        return $year % 400 === 0;
+    }
 
+    public function __toString(): string {
+        return $this->date->format('Y-m-d');
     }
 }
-
+$newdate = new SmartDate($argv[1]);
+echo "Дата: $newdate\n";
+echo "Выходной: ". ($newdate->isWeekend() ? 'Да' : 'Нет') . "\n";
+echo "Дней до сегодня:" . $newdate->getDistanceFromToday() . "\n";
+echo "Високосный год: " . ($newdate->isLeapYear() ? 'Да' : 'Нет') . "\n";
