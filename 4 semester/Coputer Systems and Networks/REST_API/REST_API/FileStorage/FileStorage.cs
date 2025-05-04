befbdf90a6bc5aa2e799fc044e5c1df9f6d4b6fa
Sunn4eS <sashabrazhalovich2005@gmail.com>
2025-05-05 01:37:57 +0300

    REST API

--- 4 semester/Coputer Systems and Networks/REST_API/REST_API/FileStorage/FileStorage.cs
+++ 4 semester/Coputer Systems and Networks/REST_API/REST_API/FileStorage/FileStorage.cs
@@ -2,5 +2,109 @@ namespace REST_API.FileStorage;
 
 public class FileStorage
 {
-    
+    private readonly string _storagePath;
+
+    public FileStorage(string storagePath)
+    {
+        _storagePath = storagePath;
+    }
+
+    public IResult Get(string? filepath)
+    {
+        try
+        {
+            string fullPath = Path.Combine(_storagePath, filepath ?? string.Empty);
+            if (Directory.Exists(fullPath))
+            {
+                var files = Directory.GetFiles(fullPath).Select(Path.GetFileName).ToArray();
+                var dirs = Directory.GetDirectories(fullPath).Select(Path.GetFileName).ToArray();
+                return Results.Ok(new {files,dirs});
+            }
+
+            if (!File.Exists(fullPath))
+            {
+                return Results.NotFound();
+            }
+            
+            return Results.File(fullPath, "application/octet-stream");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Results.InternalServerError(new { message = e.Message });
+        }   
+    }
+
+    public IResult Delete(string? filepath)
+    {
+        try
+        {
+            string fullPath = Path.Combine(_storagePath, filepath ?? string.Empty);
+            if (Directory.Exists(fullPath))
+            {
+                Directory.Delete(fullPath, true);
+                return Results.Ok(new {message = "Directory deleted successfully!"});
+            }
+
+            if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+                return Results.Ok(new {message = "File deleted successfully!"});
+            }
+            
+            return Results.NotFound();
+        }
+        
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Results.InternalServerError(new { message = e.Message });
+        }
+    }
+
+    public async Task<IResult> Put(string filepath, HttpContext content)
+    {
+        try
+        {
+            string fullPath = Path.Combine(_storagePath, filepath);
+            //
+            
+            if (content.Request.Headers.TryGetValue("X-Copy-From", out var origin))
+            {
+                string? sourcePath = Path.Combine(_storagePath, origin!);
+            
+                // если файл-источник не найден 
+                if (!File.Exists(sourcePath))
+                {
+                    return Results.NotFound(new {message = "source file not found"});
+                }
+            
+                // если файл-источник найден, то копируем
+                File.Copy(sourcePath, fullPath, true);
+                return Results.Ok(new {message = "File uploaded successfully"});
+            }
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath)!);
+            using var stream = new FileStream(fullPath, FileMode.Create);
+            await content.Request.Body.CopyToAsync(stream);
+            return Results.Created($"/{_storagePath}/{filepath}", new { message = "File uploaded successfully!" });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Results.InternalServerError(new { message = e.Message });
+        }
+    }
+
+    public IResult Head(HttpContext context, string? filepath)
+    {
+        string fullPath = Path.Combine(_storagePath, filepath ?? string.Empty);
+        if (!File.Exists(fullPath))
+        {
+            return Results.NotFound();
+        }
+        var fileInfo = new FileInfo(fullPath);
+        context.Response.Headers["Content-Length"] = fileInfo.Length.ToString();
+        context.Response.Headers["Last-Modified"] = fileInfo.LastWriteTime.ToString("R");
+        return Results.Ok(new {fileInfo});
+    }
 }
--- 4 semester/Coputer Systems and Networks/REST_API/REST_API/Program.cs
+++ 4 semester/Coputer Systems and Networks/REST_API/REST_API/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using REST_API.FileStorage;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "storage"
+});
+var app = builder.Build();
+Directory.CreateDirectory("storage");
+string strpath = Path.Combine(Directory.GetCurrentDirectory(), "storage");
+FileStorage storage = new FileStorage(strpath);
+
+app.MapGet("/{**path}",(string? path) => storage.Get(path));
+app.MapPut("/{**path}", async (string path, HttpContext context) => await storage.Put(path, context));
+app.MapDelete("/{**path}", (string? path) => storage.Delete(path));
+app.MapMethods("/{**path}", new [] {"HEAD"}, (HttpContext context, string path) => storage.Head(context, path));
 
 app.Run();
